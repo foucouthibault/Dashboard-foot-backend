4034ea981328d11f4b43d21f4a6b4aa85abbe115
thibaultfoucou <thibault@ledgity.com>
2026-07-29 23:40:36 +0200

    Add caching support with Caffeine for competition data

--- src/main/java/com/example/Dashboard_foot/DashboardFootApplication.java
+++ src/main/java/com/example/Dashboard_foot/DashboardFootApplication.java
@@ -2,8 +2,10 @@ package com.example.Dashboard_foot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class DashboardFootApplication {
 
 	public static void main(String[] args) {
